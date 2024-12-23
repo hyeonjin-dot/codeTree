40fd67e5e8a4fa0b871a8c0aca67dc9eb4b47fbc
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2024-12-23 14:01:37 +0900

    Title: 비오는 날, Time: 301ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241223/비오는 날/rainy-day.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+public class Main {
+    private static List<Info> lst = new ArrayList<>();
+
+
+    public static class Info{
+        public int year;
+        public int mon;
+        public int date;
+        public String day;
+        public String weather;
+
+        public Info(int year, int mon,int date,
+                         String day, String weather){
+            this.year = year;
+            this.mon = mon;
+            this.date = date;
+            this.day = day;
+            this.weather = weather;
+        }
+    }
+
+    public static void input(String tmp){
+        int year = Integer.parseInt(tmp.substring(0, 4));
+        int mon = Integer.parseInt(tmp.substring(5, 7));
+        int date = Integer.parseInt(tmp.substring(8, 10));
+        String day = tmp.substring(11, 14);
+        String weather = tmp.substring(15);
+
+        if (!weather.equals("Rain"))
+            return ;
+
+        Info info = new Info(year, mon, date, day, weather);
+        lst.add(info);
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int len = Integer.parseInt(sc.nextLine().trim());
+        for (int i = 0 ; i < len; i++){
+            String tmp = sc.nextLine().trim();
+            input(tmp);
+        }
+
+        Collections.sort(lst, Comparator
+                        .comparingInt((Info info)->info.year)
+                        .thenComparingInt((Info info)->info.mon)
+                        .thenComparingInt((Info info)->info.date));
+
+        if (lst.get(0).mon < 10)
+            System.out.print(lst.get(0).year + "-0" + lst.get(0).mon + "-" + 
+                    lst.get(0).date + " " 
+                    + lst.get(0).day + " "+ lst.get(0).weather);
+        else
+            System.out.print(lst.get(0).year + "-" + lst.get(0).mon + "-" + 
+                    lst.get(0).date + " " 
+                    + lst.get(0).day + " "+ lst.get(0).weather);
+    }
+}

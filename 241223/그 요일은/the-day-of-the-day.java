d5c3dd2c98a6fac3e07ff81434f76da8c6b2d69c
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2024-12-23 14:34:44 +0900

    Title: 그 요일은, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241223/그 요일은/the-day-of-the-day.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int firDate = d1;
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        int secDate = d2;
+        String date = sc.nextLine().trim();
+
+        int[] mon = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String week = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+        int weekNum;
+
+        for (int i = 0; i < 7; i++){
+            if (date.equals(week[i])){
+                weekNum = i;
+                break;
+            }
+        }
+        
+        for (int i = 0; i < m1 - 1; i++)
+            firDate += mon[i];
+
+        for (int i = 0; i < m2 - 1; i++)
+            secDate += mon[i];
+
+        int diff = secDate - firDate;
+
+        int res = diff / 7;
+        if (weekNum > 0)
+            res++;
+        System.out.print(res);
+
+    }
+}

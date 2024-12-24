9112db8639fd982a26ce2d0a6eb30ed33d35bcb2
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2024-12-24 21:16:14 +0900

    Title: 신기한 타일 뒤집기, Time: 206ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241224/신기한 타일 뒤집기/strange-flipping-tiles.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class Main {
+    public static int[] tiles = new int[101];
+    public static int black;
+    public static int white;
+
+    public static class Word{
+        int cnt;
+        int way;
+
+        public Word(int cnt, String s){
+            this.cnt = cnt;
+            if (s.equals("L"))
+                this.way = -1;
+            else
+                this.way = 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        List<Word> lst = new ArrayList<>();
+        for (int i = 0; i < n; i++){
+            String tmp = sc.nextLine().trim();
+            int cnt = Integer.parseInt(tmp.split(" ")[0]);
+            String way = tmp.split(" ")[1];
+            Word word = new Word(cnt, way);
+            lst.add(word);
+        }
+
+        black = 0;
+        white = 0;
+
+        for (int i = 0; i < n; i++){
+            if (i == 0){
+                if (lst.get(i).way == 1)
+                    black += lst.get(i).cnt;
+            } else if (lst.get(i).way == 1){
+                black += lst.get(i).cnt;
+                white -= lst.get(i).cnt;
+                if (white < 0)
+                    white = 0;
+            } else{
+                white += lst.get(i).cnt;
+                black -= lst.get(i).cnt;
+                if (black < 0)
+                    black = 0;
+            }
+        }
+
+        System.out.print(white + " " + black);
+    }
+}

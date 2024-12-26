720674b2b0219c34fa423bee7bd9f28145c25540
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2024-12-26 13:47:08 +0900

    Title: 흰검 칠하기, Time: 137ms, Memory: 12MB, Status: Wrong Answer - CodeTree

---
+++ 241226/흰검 칠하기/painting-white-black.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+public class Main {
+    public static int[] ck = new int[200001];
+    public static int[] color = new int[200001];
+
+    public static class Word{
+        int cnt;
+        int way;
+
+        public Word(int cnt, String s){
+            this.cnt = cnt;
+            if (s.equals("L"))
+                this.way = -1;
+            else
+                this.way = 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = Integer.parseInt(sc.nextLine().trim());
+
+        List<Word> lst = new ArrayList<>();
+        for (int i = 0; i < n; i++){
+            String tmp = sc.nextLine().trim();
+            int cnt = Integer.parseInt(tmp.split(" ")[0]);
+            String way = tmp.split(" ")[1];
+            Word word = new Word(cnt, way);
+            lst.add(word);
+        }
+
+
+        int idx = 100000;
+        int tmp_way = 0;
+        for (Word word : lst){
+            if (tmp_way == 0){
+                for (int i = 0; i < word.cnt; i++){
+                    ck[idx]++;
+                    if(ck[idx] >= 4)
+                        color[idx] = 0;
+                    else
+                        color[idx] = word.way;
+                    idx += word.way;
+                }
+            }else {
+                if (tmp_way != word.way){
+                    idx += word.way;
+                    for (int i = 0; i < word.cnt; i++){
+                        ck[idx]++;
+                        if(ck[idx] >= 4)
+                            color[idx] = 0;
+                        else
+                            color[idx] = word.way;
+                        idx += word.way;
+                    }
+                }
+            }
+            tmp_way = word.way;
+        }
+
+        int gray = 0;
+        int black = 0;
+        int white = 0;
+        for (int i = 0; i < 200001; i++){
+            if (ck[i] >= 4)
+                gray++;
+            if (color[i] == 1)
+                black++;
+            else if (color[i] == -1)
+                white++;
+        }
+
+        System.out.print(white + " " + black + " " + gray);
+    }
+}

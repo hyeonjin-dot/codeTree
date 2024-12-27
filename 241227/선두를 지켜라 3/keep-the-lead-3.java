8d8de947c979347bd589fbf5275e8f6e5c5a9ca2
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2024-12-27 14:17:42 +0900

    Title: 선두를 지켜라 3, Time: 214ms, Memory: 27MB, Status: Passed - CodeTree

--- 241227/선두를 지켜라 3/keep-the-lead-3.java
+++ 241227/선두를 지켜라 3/keep-the-lead-3.java
@@ -1,56 +1,58 @@
 import java.util.*;
 
 public class Main {
-    public static List<Integer> A = new ArrayList<>();
-    public static List<Integer> B = new ArrayList<>();
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int a_len = sc.nextInt();
         int b_len = sc.nextInt();
 
-        for (int i = 0; i < a_len; i++){
+        int[] a_cumulative = new int[1000001];
+        int[] b_cumulative = new int[1000001];
+
+        int maxA = 0, maxB = 0;
+        int idx = 0;
+        for (int i = 0; i < a_len; i++) {
             int v = sc.nextInt();
             int t = sc.nextInt();
 
-            if (A.size() == 0)
-                A.add(v);
-            else{
-                for (int j = 1; j < t; j++)
-                    A.add(Collections.max(A) + v);
+            for (int j = 0; j < t; j++) {
+                maxA += v;
+                a_cumulative[idx] = maxA;
+                idx++;
             }
         }
 
-        for (int i = 0; i < b_len; i++){
+        idx = 0;
+        for (int i = 0; i < b_len; i++) {
             int v = sc.nextInt();
             int t = sc.nextInt();
 
-            if (B.size() == 0)
-                B.add(v);
-            else{
-                for (int j = 0; j < t; j++)
-                    B.add(Collections.max(B) + v);
+            for (int j = 0; j < t; j++) {
+                maxB += v;
+                b_cumulative[idx] = maxB;
+                idx++;
             }
         }
 
-        int ret = 0;
-        int tmp_ret;
         int prt = 0;
-        for (int i = 0; i < A.size(); i++){
-            tmp_ret = ret;
-            // System.out.println(A.get(i) + " " + B.get(i));
-            if (A.get(i) > B.get(i))
-                ret = 1;
-            else if (A.get(i) < B.get(i))
-                ret = 2;
-            else
-                ret = 3;
+        int lastResult = 0;
+        for (int i = 0; i < idx; i++) {
+            int result;
+            if (a_cumulative[i] > b_cumulative[i]) {
+                result = 1;
+            } else if (a_cumulative[i] < b_cumulative[i]) {
+                result = 2;
+            } else {
+                result = 3;
+            }
 
-            if (tmp_ret != ret)
+            if (result != lastResult) {
                 prt++;
+                lastResult = result;
+            }
         }
 
-        System.out.print(prt);
+        System.out.println(prt);
     }
 }

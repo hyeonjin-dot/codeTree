c74a742126213d441580b9f659f01b730c31ab6e
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2024-12-30 21:13:02 +0900

    Title: Carry 피하기 2, Time: 135ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241230/Carry 피하기 2/escaping-carry-2.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] lst = new int[n];
+
+        for (int i = 0; i < n; i++){
+            int tmp = sc.nextInt();
+            lst[i] = tmp;
+        }
+
+        int max = 0;
+
+        for (int i = 0; i < n - 2; i++){
+            for (int j = 1; j < n - 1; j++){
+                for (int k = 2; k < n; k++){
+                    if ((lst[i] % 10) + (lst[j] % 10) +(lst[k] % 10) < 10){
+                        if (lst[i] % 100 + lst[j] % 100 + lst[k] % 100 < 100){
+                            if (lst[i] % 1000 + lst[j] % 1000 
+                            + lst[k] % 1000 < 1000){
+                                if (lst[i] % 10000 + lst[j] % 10000 
+                            + lst[k] % 10000 < 10000){
+                                int tmp = lst[i] + lst[j] + lst[k];
+                                max = max > tmp ? max : tmp;
+                            }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.print(max);
+    }
+}

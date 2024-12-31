bac82387d6cfa6dec2dd1fd90284378d0fa79e48
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2024-12-31 17:32:26 +0900

    Title: 오목, Time: 140ms, Memory: 11MB, Status: Wrong Answer - CodeTree

--- 241231/오목/O-mok.java
+++ 241231/오목/O-mok.java
@@ -5,6 +5,8 @@ public class Main {
     // public static boolean[][] visited = new int[19][19];
     public static int black_max = 0;
     public static int white_max = 0;
+    public static int[] black = new int[2];
+    public static int[] white = new int[2];
     public static int win = 0;
 
     public static int right(int x, int y) { // y++
@@ -13,6 +15,15 @@ public class Main {
             cnt++;
             y++;
         }
+
+        if (cnt == 5 && board[x][y] == 1){
+            white[0] = x;
+            white[1] = y;
+        }else if (cnt == 5){
+            black[0] = x;
+            black[1] = y;
+        }
+
         return cnt;
     }
 
@@ -22,15 +33,43 @@ public class Main {
             cnt++;
             x++;
         }
+                
+        if (cnt == 5 && board[x][y] == 1){
+            white[0] = x;
+            white[1] = y;
+        }else if (cnt == 5){
+            black[0] = x;
+            black[1] = y;
+        }
+
         return cnt;
     }
 
-    public static int diagonal(int x, int y) { // x++, y++
+    public static int diagonalRight(int x, int y) { // x++, y++
         int cnt = 1;
         while (x + 1 < 19 && y + 1 < 19 && board[x][y] == board[x + 1][y + 1]) {
             cnt++;
             x++;
             y++;
+        }
+        
+        if (cnt == 5 && board[x][y] == 1){
+            white[0] = x;
+            white[1] = y;
+        }else if (cnt == 5){
+            black[0] = x;
+            black[1] = y;
+        }
+
+        return cnt;
+    }
+
+    public static int diagonalLeft(int x, int y) { // x++, y--
+        int cnt = 1;
+        while (x + 1 < 19 && y - 1 > -1 && board[x][y] == board[x + 1][y - 1]) {
+            cnt++;
+            x++;
+            y--;
         }
         return cnt;
     }
@@ -52,9 +91,11 @@ public class Main {
                 else{
                     int rightCnt = right(i, j);
                     int downCnt = down(i, j);
-                    int diagonalCnt = diagonal(i, j);
+                    int diagonalRightCnt = diagonalRight(i, j);
+                    int diagonalLeftCnt = diagonalLeft(i, j);
                     rightCnt = rightCnt > downCnt ? rightCnt : downCnt;
-                    rightCnt = rightCnt > diagonalCnt ? rightCnt : diagonalCnt;
+                    diagonalRightCnt = diagonalRightCnt > diagonalLeftCnt ? diagonalRightCnt : diagonalLeftCnt;
+                    rightCnt = rightCnt > diagonalRightCnt ? rightCnt : diagonalRightCnt;
                     if (board[i][j] == 1)
                         black_max = black_max > rightCnt ? black_max : rightCnt;
                     else if (board[i][j] == 2)
@@ -63,12 +104,15 @@ public class Main {
             }
         }
 
-        if (black_max == 5)
-            win = 1;
-        else if (white_max == 5)
-            win = 2;
-        System.out.println(win);
-        System.out.print(black_max + " " + white_max);
+        if (black_max == 5){
+            System.out.println(1);
+            System.out.print(white[0] - 1 + " " + (white[1] - 1));
+        }
+        else if (white_max == 5){
+            System.out.println(2);
+            System.out.print(black[0] - 1 + " " + (black[1] - 1));
+        }else
+            System.out.print(0);
         
     }
 }

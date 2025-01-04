88f66038a8e66798b2ca788693ee475a3599dade
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-01-04 21:37:33 +0900

    Title: 스승의 은혜 2, Time: 118ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 250104/스승의 은혜 2/the-grace-form-teacher-2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int b = sc.nextInt();
+
+        int[] lst = new int[n];
+
+        for (int i = 0; i < n; i++)
+            lst[i] = sc.nextInt();
+
+        Arrays.sort(lst);
+
+        int sum = 0;
+        int idx = -1;
+        boolean used = false;
+        while (idx < n && sum <= b){
+            idx++;
+            if (sum + lst[idx] > b && !used){
+                used = true;
+                sum += lst[idx] / 2 ;
+            }else
+                sum += lst[idx];
+        }
+
+        System.out.print(idx);
+    }
+}

c720cc867f4499abb6d3fa0557c60cf12ab1b1bd
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-01-04 22:05:59 +0900

    Title: 상해버린 치즈, Time: 166ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 250104/상해버린 치즈/rotten-cheese.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int d = sc.nextInt();
+        int s = sc.nextInt();
+
+        int [][] taken = new int[n][m]; // n번 사람이 m번 치즈를 몇초에 먹었는지
+        int [] ill = new int[n]; // n번째 사람이 언제 아프기 시작했는지 
+        int [] medicine = new int[n]; // 약먹어야하면 추가
+        boolean [] spoil = new boolean[m]; // 치즈가 상했나요
+
+        for (int i = 0; i < d; i++){
+            int person = sc.nextInt() - 1;
+            int cheese = sc.nextInt() - 1;
+            int when = sc.nextInt();
+            taken[person][cheese] = when;
+        }
+
+        for (int i = 0; i < s; i++){
+            int person = sc.nextInt() - 1;
+            int start = sc.nextInt();
+            ill[person] = start;
+        }
+
+        for (int i = 0; i < n; i++){ // 사람 넘버
+            if (ill[i] == 0)
+                continue;
+
+            for (int j = 0; j < m; j++){
+                if (taken[i][j] < ill[i])
+                    spoil[j] = true;
+            }
+
+        }
+
+        for (int i = 0; i < m; i++){
+            if (!spoil[i])
+                continue;
+
+            for (int j = 0; j < n; j++){
+                if (taken[j][i] > 0)
+                    medicine[j] = 1;
+            }
+        }
+
+        int res = 0;
+        for (int i = 0; i < n; i++){
+            if (medicine[i] == 1)
+                res++;
+        }
+
+        System.out.print(res);
+    }
+}

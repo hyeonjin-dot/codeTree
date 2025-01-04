3d3d586e1a7a12f76e57e8c4b298cf9a8c09a4ab
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-01-04 23:18:50 +0900

    Title: 이상한 폭탄 2, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250104/이상한 폭탄 2/strange-bomb-2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] lst = new int[n];
+
+        for (int i = 0; i < n; i++)
+            lst[i] = sc.nextInt();
+        
+        int max = -1;
+        for (int i = 0; i < n - k; i++){
+            for (int j = i + 1; j <= i + k && j < n; j++){
+                if (lst[i] == lst[j])
+                    max = Math.max(lst[i], max);
+            }
+        }
+
+        System.out.print(max);
+    }
+}

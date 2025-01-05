ba2b850bd45d9ca764fff75f786da13063da04de
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-01-05 22:52:25 +0900

    Title: 스승의 은혜 3, Time: 252ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250105/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int money = sc.nextInt();
+
+        int[][] lst = new int[n][3];
+
+        for (int i = 0; i < n; i++){
+            lst[i][0] = sc.nextInt();
+            lst[i][1] = sc.nextInt();
+            lst[i][2] = lst[i][0] + lst[i][1];
+        }
+        Arrays.sort(lst, (a, b) -> {
+            if (a[2] != b[2]) {
+                return Integer.compare(a[2], b[2]); // a[2] 기준
+            } else {
+                return Integer.compare(b[0], a[0]);
+            }
+        });
+
+        boolean used = false;
+        int sum = 0;
+        int idx = 0;
+        while (sum <= money && idx < n){
+            sum += lst[idx][1];
+
+            if (!used && sum + lst[idx][0] > money 
+                    && sum + (lst[idx][0] / 2) < money){
+                sum += lst[idx][0] / 2;
+                used = true;
+            }else
+                sum += lst[idx][0];
+            idx++;
+        }
+
+        if (sum <= money)
+            System.out.print(idx);
+        else
+            System.out.print(idx - 1);
+    }
+}

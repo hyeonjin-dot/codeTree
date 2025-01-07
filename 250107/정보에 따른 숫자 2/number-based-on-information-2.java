10b729783d3d547ea0223dbd59c5b9df4f4dfa2c
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-01-07 21:20:26 +0900

    Title: 정보에 따른 숫자 2, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250107/정보에 따른 숫자 2/number-based-on-information-2.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sc.nextLine();
+
+        int[] lst = new int[1001];
+
+        for (int i = 0; i < t; i++){
+            String tmp = sc.nextLine().trim();
+            if (tmp.charAt(0) == 'S')
+                lst[Integer.parseInt(tmp.substring(2))] = 1;
+            else
+                lst[Integer.parseInt(tmp.substring(2))] = 2;
+        }
+        
+        int res = 0;
+
+        for (int i = a; i <= b; i++){
+            int up = 1001;
+            int down = 1001;
+            for (int j = i; j >= 0; j--){
+                if (lst[j] == 1){
+                    down = (i - j);
+                    break;
+                }
+            }
+            for (int j = i; j <= b; j++){
+                if (lst[j] == 1){
+                    up = (j - i);
+                    break;
+                }
+            }
+            int ds = Math.min(up, down);
+            up = 1001;
+            down = 1001;
+            for (int j = i; j >= 0; j--){
+                if (lst[j] == 2){
+                    down = (i - j);
+                    break;
+                }
+            }
+            for (int j = i; j <= b; j++){
+                if (lst[j] == 2){
+                    up = (j - i);
+                    break;
+                }
+            }
+            int dn = Math.min(up, down);
+            if (ds <= dn)
+                res++;
+        }
+
+        System.out.print(res);
+    }
+}

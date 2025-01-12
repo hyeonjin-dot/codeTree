372ef9a032d8805a41e6f6345849a51b1eefbe4e
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-01-12 12:42:41 +0900

    Title: 2개씩 그룹짓기 2, Time: 340ms, Memory: 23MB, Status: Wrong Answer - CodeTree

--- 250112/2개씩 그룹짓기 2/group-of-pairs-2.java
+++ 250112/2개씩 그룹짓기 2/group-of-pairs-2.java
@@ -13,12 +13,12 @@ public class Main {
 
         Arrays.sort(lst); // 정렬
 
-        long minMax = Long.MIN_VALUE; // 최솟값 중 최댓값 저장
+        long minMax = Long.MAX_VALUE; // 최솟값 중 최댓값 저장
 
         // i번째 작은 값과 n/2 + i번째 큰 값을 비교
         for (int i = 0; i < n / 2; i++) {
             long diff = lst[n / 2 + i] - lst[i];
-            minMax = Math.max(minMax, diff); // 최솟값 중 최대를 갱신
+            minMax = Math.min(minMax, diff); // 최솟값 중 최대를 갱신
         }
 
         System.out.print(minMax);

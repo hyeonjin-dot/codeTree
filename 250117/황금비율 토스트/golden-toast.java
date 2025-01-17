bdc25a3bd85f3f4aa2211a4082a6e8c9816d8f54
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-01-17 16:25:44 +0900

    Title: 황금비율 토스트, Time: 152ms, Memory: 12MB, Status: Passed - CodeTree

--- 250117/황금비율 토스트/golden-toast.java
+++ 250117/황금비율 토스트/golden-toast.java
@@ -1,45 +1,41 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        // BufferedReader와 StringBuilder 사용
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken()); // 초기 문자열 길이
+        int m = Integer.parseInt(st.nextToken()); // 명령어 개수
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        sc.nextLine();
-
-        LinkedList<Character> lst = new LinkedList<>();
-        for (char c : sc.nextLine().trim().toCharArray()) {
-            lst.add(c);
-        }
-
-        ListIterator<Character> it = lst.listIterator(lst.size());
+        StringBuilder sb = new StringBuilder(br.readLine()); // 초기 문자열
+        int cursor = n; // 초기 커서는 문자열 끝
 
         for (int i = 0; i < m; i++) {
-            String command = sc.nextLine().trim();
+            String command = br.readLine();
             char action = command.charAt(0);
+
             switch (action) {
-                case 'L':
-                    if (it.hasPrevious()) it.previous();
+                case 'L': // 커서를 왼쪽으로 이동
+                    if (cursor > 0) cursor--;
                     break;
-                case 'R':
-                    if (it.hasNext()) it.next();
+                case 'R': // 커서를 오른쪽으로 이동
+                    if (cursor < sb.length()) cursor++;
                     break;
-                case 'D':
-                    if (it.hasNext()) {
-                        it.next();
-                        it.remove();
-                    }
+                case 'D': // 현재 커서 위치의 문자 삭제
+                    if (cursor < sb.length()) sb.deleteCharAt(cursor);
                     break;
-                case 'P':
-                    String s = command.split(" ")[1];
-                    it.add(s.charAt(0));
+                case 'P': // 현재 커서 위치에 문자 삽입
+                    char c = command.split(" ")[1].charAt(0);
+                    sb.insert(cursor, c);
+                    cursor++;
                     break;
             }
         }
 
-        it = lst.listIterator();
-        while (it.hasNext())
-            System.out.print(it.next());
+        // 최종 결과 출력
+        System.out.print(sb);
     }
 }

8ae5fb8ada9194f3c1e0115b0b1e43c568ac910e
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-01-20 13:58:07 +0900

    Title: 기수 정렬 구현, Time: 1469ms, Memory: 38MB, Status: Passed - CodeTree

--- 250120/기수 정렬 구현/implement-radix-sort.java
+++ 250120/기수 정렬 구현/implement-radix-sort.java
@@ -1,30 +1,28 @@
 import java.util.*;
 
 public class Main {
-    public static int[] radix(int[] lst, int k){
+    public static int[] radix(int[] lst, int k) {
         int n = lst.length;
 
-        for (int i = k - 1; i >= 0; i--){
-            int[][] num = new int[10][n];
-            for (int j = 0; j < n; j++){
-                int digit = lst[j] % (int)Math.pow(10, k - i);
-                num[digit][j] = lst[j];
-            }
-            int cnt = 0;
-            int[] tmp = new int[n];
-            int digit = 0;
-            int j = 0;
-            while (cnt < n){
-                if (num[digit][j] > 0)
-                    tmp[cnt++] = num[digit][j];
-                j++;
-                if (j == n){
-                    j = 0;
-                    digit++;
-                }
+        for (int i = 0; i < k; i++) {
+            Queue<Integer>[] num = new LinkedList[10];
+            for (int d = 0; d < 10; d++) {
+                num[d] = new LinkedList<>();
             }
 
-            lst = tmp;
+            // 자릿수별로 분배
+            for (int j = 0; j < n; j++) {
+                int digit = (lst[j] / (int) Math.pow(10, i)) % 10;
+                num[digit].offer(lst[j]);
+            }
+
+            // 새로운 배열로 재구성
+            int cnt = 0;
+            for (int d = 0; d < 10; d++) {
+                while (!num[d].isEmpty()) {
+                    lst[cnt++] = num[d].poll();
+                }
+            }
         }
 
         return lst;

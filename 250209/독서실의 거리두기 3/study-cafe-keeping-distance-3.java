6ad5705228881d8fc1b86904fc584d4f167b68f5
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-02-09 22:01:45 +0900

    Title: 독서실의 거리두기 3, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250209/독서실의 거리두기 3/study-cafe-keeping-distance-3.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        String input = sc.nextLine().trim();
+        int[] seat = new int[n];
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int before = 0;
+        for (int i = 0; i < n; i++){
+            seat[i] = (int)(input.charAt(i) - '0');
+            if (i > 0 && input.charAt(i) == '1'){
+                min = Math.min(min, i - before);
+                max = Math.max(max, i - before);
+                before = i;
+            }
+        }
+
+        System.out.print(max - 1);
+        
+        
+    }
+}

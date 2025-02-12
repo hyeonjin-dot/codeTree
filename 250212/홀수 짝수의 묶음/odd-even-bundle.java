f6cbdddd36be59240170506e21ddbfb6d3eb9d1f
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-02-12 15:39:46 +0900

    Title: 홀수 짝수의 묶음, Time: 126ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 250212/홀수 짝수의 묶음/odd-even-bundle.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] lst = new int[n];
+        List<Integer> odd = new ArrayList<>();
+        List<Integer> even = new ArrayList<>();
+
+        for (int i = 0; i < n; i++){
+            lst[i] = sc.nextInt();
+            if (lst[i] % 2 == 1)
+                odd.add(lst[i]);
+            else
+                even.add(lst[i]);
+        }
+        int res = 0;
+
+        if (odd.size() == even.size()){
+            System.out.print(n);
+            return ;
+        }
+
+        int cnt = 0;
+        while (odd.size() + even.size() > 0){
+            if (cnt % 2 == 0){
+                if (even.size() > 0)
+                    even.remove(0);
+                else if (odd.size() >= 2) {
+                    odd.remove(0);
+                    odd.remove(0);
+                }
+                else if (odd.size() == 1) {
+                    res -= 2;
+                    odd.remove(0);
+                }
+
+            }else{
+                odd.remove(0);
+            }
+
+            cnt++;
+            res++;
+        }
+
+        System.out.print(res);
+    }
+}

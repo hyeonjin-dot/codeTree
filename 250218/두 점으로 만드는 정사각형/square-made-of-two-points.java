6e454e8d0c8bb59a4918b9503e8b773dd838fe4a
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-02-18 14:52:32 +0900

    Title: 두 점으로 만드는 정사각형, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 250218/두 점으로 만드는 정사각형/square-made-of-two-points.java
+++ 250218/두 점으로 만드는 정사각형/square-made-of-two-points.java
@@ -14,21 +14,18 @@ public class Main {
         int xx2 = sc.nextInt();
         int yy2 = sc.nextInt();
 
-        int x = 0;
-        int y = 0;
-        if (x2 > xx2)
-            x = x2 - xx1;
-        else
-            x = xx2 - x1;
-        
-        if (y2 > yy2)
-            y = y2 - yy1;
-        else
-            y = yy2 - y1;
+        int xmax = Math.max(xx2, x2);
+        int xmin = Math.min(x1, xx1);
+        int ymax = Math.max(yy2, y2);
+        int ymin = Math.min(y1, yy1);
+
+        int x = xmax - xmin;
+        int y = ymax - ymin;
 
         if (x > y)
-            System.out.print(x * x);
+            System.our.print(x * x);
         else
-            System.out.print(y * y);
+            System.our.print(y * y);
+      
     }
 }

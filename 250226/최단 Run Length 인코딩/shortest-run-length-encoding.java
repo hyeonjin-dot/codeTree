c7d95a81e92ca75b503e79b8a99d1bbe9fedc640
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-02-26 14:40:36 +0900

    Title: 최단 Run Length 인코딩, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250226/최단 Run Length 인코딩/shortest-run-length-encoding.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next().trim();
+
+        int len = A.length();
+        
+        boolean vaild = true;
+        for (int i = 0; i < len - 1; i++){
+            if (A.charAt(i) != A.charAt(i + 1)){
+                vaild = false;
+                break ;
+            }
+        }
+
+        if (vaild){
+            if (len < 10)
+                System.out.print(2);
+            else
+                System.out.print(3);
+            return ;
+        }
+
+        int cnt = 0;
+        for (int i = len - 1; i > 0; i--){
+            if (A.charAt(i) == A.charAt(0))
+                cnt++;
+        }
+
+        int diff = 0;
+        int res = 2;
+        for (int i = 1; i < len - cnt; i++){
+            if (A.charAt(i) != A.charAt(i - 1) || i == len - cnt - 1){
+                res += 2;
+            }
+        }
+
+        System.out.print(res);
+    }
+}

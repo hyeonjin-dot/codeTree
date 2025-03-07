8ca246a80660f3ac099c3e3f5af0e4078264a8d0
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-03-07 18:57:49 +0900

    Title: 등차수열, Time: 172ms, Memory: 10MB, Status: Passed - CodeTree

--- 250307/등차수열/arithmetic-sequence.java
+++ 250307/등차수열/arithmetic-sequence.java
@@ -4,24 +4,6 @@ public class Main {
     public static List<Integer> arr = new ArrayList<>();
     public static int n;
 
-    public static int moreIdx(int target){
-        for (int i = 0; i < n; i++){
-            if (arr.get(i) > target)
-                return i;
-        }
-
-        return -1;
-    }
-
-    public static int lessIdx(int target){
-        for (int i = n - 1; i >= 0; i--){
-            if (arr.get(i) < target)
-                return i;
-        }
-
-        return -1;
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
@@ -35,14 +17,12 @@ public class Main {
         
         int res = 0;
 
-        for (int i = arr.get(0) + 1; i < arr.get(n - 1); i++){
-            if (arr.contains(i))
-                continue;
+        for (int i = 0; i <= 101; i++){
             int cnt = 0;
-            for (int j = 0; j <= lessIdx(i); j++){
-                int diff = i - arr.get(j);
-                for (int k = moreIdx(i); k < n; k++){
-                    if (diff == (arr.get(k) - i))
+
+            for (int x = 0; x < n; x++){
+                for (int y = x + 1; y < n; y++){
+                    if (i - arr.get(x) == arr.get(y) - i)
                         cnt++;
                 }
             }

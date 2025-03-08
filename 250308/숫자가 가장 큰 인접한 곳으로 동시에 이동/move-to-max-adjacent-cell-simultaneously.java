afbfffe7b7edb28f9d62656b52f16904c8d1f499
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-03-08 16:55:19 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 127ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 250308/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.java
@@ -0,0 +1,92 @@
+import java.util.*;
+public class Main {
+    public static int n;
+    public static int m;
+    public static int[][] grid;
+    public static int[][] marbles;
+    public static int[] check;
+    public static int[] dx = {-1, 1, 0, 0};
+    public static int[] dy = {0, 0, -1, 1};
+
+    public static boolean inRange(int x, int y){
+        return (x >= 0 && x < n && y >= 0 && y < n);
+    }
+
+    public static void same(){
+        for (int i = 0; i < m; i++){
+            if (check[i] > 0)
+                    continue ;
+
+            for (int j = 0; j < m; j++){
+                if (i == j)
+                    continue ;
+ 
+                if (marbles[i][0] == marbles[j][0] 
+                    && marbles[i][1] == marbles[j][1]){
+                        check[j]++;
+                        check[i]++;
+                    }
+            }
+        }
+    }
+
+    public static void move(){
+        for (int i = 0; i < m; i++){
+            if (check[i] > 0)
+                continue ;
+            
+            // System.out.print(i + " ");
+            // System.out.println(marbles[i][0] + " " + marbles[i][1]);
+            int x = marbles[i][0];
+            int y = marbles[i][1];
+
+            int idx = 0;
+            for (int j = 0; j < 4; j++){
+                if (!inRange(x + dx[j], y + dy[j]))
+                    continue;
+                
+                if (grid[x + dx[idx]][y + dy[idx]] 
+                        < grid[x + dx[j]][y + dy[j]])
+                    idx = j;
+                
+            }
+            marbles[i][0] = x + dx[idx];
+            marbles[i][1] = y + dy[idx];
+            // System.out.print(i + " ");
+            // System.out.println(marbles[i][0] + " " + marbles[i][1]);
+        }
+        same();
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt(); // 구슬 개수
+        int t = sc.nextInt(); // 시간
+        grid = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                grid[i][j] = sc.nextInt();
+        marbles = new int[m][2];
+        for (int i = 0; i < m; i++) {
+            marbles[i][0] = sc.nextInt() - 1;
+            marbles[i][1] = sc.nextInt() - 1;
+        }
+
+        // 두개 이상이 같은 위치가 되면 사라짐
+        check = new int[m];
+        for (int i = 0; i < t; i++){
+            // System.out.println(i);
+            move();
+        }
+
+        int res = 0;
+        for (int i = 0; i < m; i++){
+            if (check[i] == 0)
+                res++;
+        }
+
+        System.out.print(res);
+    }
+}

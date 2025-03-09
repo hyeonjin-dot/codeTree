6f0218a9fa840c3984b97afb8842e734c0675e72
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-03-09 15:23:36 +0900

    Title: 트로미노, Time: 135ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250309/트로미노/tromino.java
+++ 250309/트로미노/tromino.java
@@ -1,112 +1,78 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static int[][] grid;
     public static int n;
     public static int m;
 
-    public static int[] lineX = {0, 1, 2}; // 가로 X만
-    public static int[] lineY = {0, 1, 2}; // 가로 y만
+    public static int[] dx = {0, 1, 2, 0, 0, -1, -2, 0};
+    public static int[] dy = {0, 0, 0, 1, 2, 0, 0, -1};
 
-    public static int[] oneX = {-1, 0, 0};
-    public static int[] oneY = {0, 0, 1};
+    public static int[] ldx = {0, 1, 1, 1, 0, -1, -1, -1};
+    public static int[] ldy = {1, 0, -1, 1, -1, 1, 0, -1};
 
-    public static int[] twoX = {0, 0, 1};
-    public static int[] twoY = {0, 1, 0};
-
-    public static int[] threeX = {0, 0, 1};
-    public static int[] threeY = {-1, 0, 0};
-
-    public static int[] fourX = {-1, 0, 0};
-    public static int[] fourY = {0, 0, -1};
-
-    public static boolean inRange(int x, int y){
-        return (x >= 0 && x < n && y < n && y >= 0);
+    public static boolean inRange(int x, int y) {
+        return (x >= 0 && x < n && y >= 0 && y < m);
     }
 
-    public static int max(){
+    public static int maxSum() {
         int ret = Integer.MIN_VALUE;
-        int sum = 0;
 
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < m - 2; j++)
-                sum = grid[i][j] + grid[i][j + 1] + grid[i][j + 2];                
-            ret = Math.max(ret, sum);
-        }
-
-        for (int j = 0; j < m; j++){
-            for (int i = 0; i < n - 2; i++)
-                sum = grid[i][j] + grid[i + 1][j] + grid[i + 2][j];
-            ret = Math.max(ret, sum);
-        }
-
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++){
-                if (!inRange(i + oneX[0], j + oneY[0]) 
-                || !inRange(i + oneX[1], j + oneY[1])
-                || !inRange(i + oneX[2], j + oneY[2]))
-                continue;
-
-                sum = grid[i + oneX[0]][j + oneY[0]] 
-                    + grid[i + oneX[1]][j + oneY[1]] + grid[i + oneX[2]][j + oneY[2]];
+        // 1. 가로/세로 3칸 합 계산
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m - 2; j++) {
+                int sum = grid[i][j] + grid[i][j + 1] + grid[i][j + 2];
                 ret = Math.max(ret, sum);
             }
         }
-        
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++){
-                if (!inRange(i + twoX[0], j + twoY[0]) 
-                || !inRange(i + twoX[1], j + twoY[1])
-                || !inRange(i + twoX[2], j + twoY[2]))
-                continue;
 
-                sum = grid[i + twoX[0]][j + twoY[0]] 
-                    + grid[i + twoX[1]][j + twoY[1]] + grid[i + twoX[2]][j + twoY[2]];
+        for (int j = 0; j < m; j++) {
+            for (int i = 0; i < n - 2; i++) {
+                int sum = grid[i][j] + grid[i + 1][j] + grid[i + 2][j];
                 ret = Math.max(ret, sum);
             }
         }
-                
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++){
-                if (!inRange(i + threeX[0], j + threeY[0]) 
-                || !inRange(i + threeX[1], j + threeY[1])
-                || !inRange(i + threeX[2], j + threeY[2]))
-                continue;
 
-                sum = grid[i + threeX[0]][j + threeY[0]] 
-                    + grid[i + threeX[1]][j + threeY[1]] + grid[i + threeX[2]][j + threeY[2]];
-                ret = Math.max(ret, sum);
+        // 2. L자형 블록 (8가지 방향 고려)
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int k = 0; k < 8; k++) {  // 8방향 탐색
+                    int sum = grid[i][j];  // 기준점 포함
+                    boolean valid = true;
+
+                    for (int d = 0; d < 3; d++) {
+                        int nx = i + dx[d] + ldx[k];
+                        int ny = j + dy[d] + ldy[k];
+
+                        if (!inRange(nx, ny)) {
+                            valid = false;
+                            break;
+                        }
+                        sum += grid[nx][ny];
+                    }
+
+                    if (valid) {
+                        ret = Math.max(ret, sum);
+                    }
+                }
             }
         }
-        
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++){
-                if (!inRange(i + fourX[0], j + fourY[0]) 
-                || !inRange(i + fourX[1], j + fourY[1])
-                || !inRange(i + fourX[2], j + fourY[2]))
-                continue;
 
-                sum = grid[i + fourX[0]][j + fourY[0]] 
-                    + grid[i + fourX[1]][j + fourY[1]] + grid[i + fourX[2]][j + fourY[2]];
-                ret = Math.max(ret, sum);
-            }
-        }
         return ret;
     }
-
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         m = sc.nextInt();
         grid = new int[n][m];
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 grid[i][j] = sc.nextInt();
             }
         }
-        
-        System.out.print(max());
 
+        System.out.println(maxSum());
     }
 }

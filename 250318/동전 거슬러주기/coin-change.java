a5db273c7a78e31ff4cc52d6959a2ff3c90d6b5e
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-03-18 19:37:39 +0900

    Title: 동전 거슬러주기, Time: 134ms, Memory: 10MB, Status: Runtime Error - CodeTree

--- 250318/동전 거슬러주기/coin-change.java
+++ 250318/동전 거슬러주기/coin-change.java
@@ -6,24 +6,24 @@ public class Main {
     public static int[] coin;
     public static int[] dp;
 
-    public static int find(int num){
-        int min = Integer.MAX_VALUE;
+    // public static int find(int num){
+    //     int min = Integer.MAX_VALUE;
 
-        for (int i = 0; i < n; i++){
-            if (num - coin[i] >= 0 && dp[num - coin[i]] != 0)
-                min = Math.min(dp[num - coin[i]], min);
-        }
-        return min;
-    }
+    //     for (int i = 0; i < n; i++){
+    //         if (num - coin[i] >= 0 && dp[num - coin[i]] != 0)
+    //             min = Math.min(dp[num - coin[i]], min);
+    //     }
+    //     return min;
+    // }
 
     public static void makeDp(){
         for (int i = 0; i < n; i++){
             for (int j = coin[i]; j <= m; j++){
                 if (dp[j - coin[i]] != 0){
                     if (dp[j] == 0)
-                        dp[j] = dp[coin[i]] + 1;
+                        dp[j] = dp[j - coin[i]] + 1;
                     else
-                        dp[j] = Math.min(dp[j], dp[coin[i]] + 1);
+                        dp[j] = Math.min(dp[j], dp[j - coin[i]] + 1);
                 }
             }
         }
@@ -43,10 +43,12 @@ public class Main {
 
         dp[0] = 0;
         makeDp();
-        int res = find(m);
-        if (res == Integer.MAX_VALUE)
-            res = -2;
-        System.out.print(res + 1);
+        // int res = find(m);
+        // if (res == Integer.MAX_VALUE)
+        //     res = -2;
+        if (dp[m] == 0)
+            dp[m] = -1;
+        System.out.print(dp[m]);
         
     }
 }

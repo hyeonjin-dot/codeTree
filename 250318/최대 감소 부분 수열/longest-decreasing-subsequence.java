3655a2d9f667dbaf331aa90a746cc290088bae8c
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-03-18 16:36:18 +0900

    Title: 최대 감소 부분 수열, Time: 170ms, Memory: 11MB, Status: Passed - CodeTree

--- 250318/최대 감소 부분 수열/longest-decreasing-subsequence.java
+++ 250318/최대 감소 부분 수열/longest-decreasing-subsequence.java
@@ -1,31 +1,37 @@
 import java.util.*;
 public class Main {
+    public static int n;
+    public static int[] arr;
+    public static int[] dp;
+
+    public static int findMax(int idx){
+        int max = 0;
+
+        for (int i = idx - 1; i >= 0; i--){
+            if (arr[idx] < arr[i])
+                max = Math.max(dp[i], max);
+        }
+
+        return max;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
+        n = sc.nextInt();
+        arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
         
         int max = 0;
 
-        int[] dp = new int[n];
+        dp = new int[n];
         dp[0] = 1;
+        
         for (int i = 1; i < n; i++){
-            if (arr[i - 1] > arr[i])
-                dp[i] = dp[i - 1] + 1;
-            else {
-                for (int j = i - 1; j >= 0; j--){
-                    if (arr[j] > arr[i]){
-                        dp[i] = dp[j] + 1;
-                        break ;
-                    }
-                }
-                if (dp[i] == 0)
-                    dp[i] = 1;
-            }
+            dp[i] = findMax(i) + 1;
         }
+
         Arrays.sort(dp);
         System.out.print(dp[n - 1]);
     }

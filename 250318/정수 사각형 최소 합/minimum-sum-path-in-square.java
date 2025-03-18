7ee07bfdfb3d72829c03a818a51d74e80cdca92e
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-03-18 16:08:24 +0900

    Title: 정수 사각형 최소 합, Time: 334ms, Memory: 21MB, Status: Passed - CodeTree

--- 250318/정수 사각형 최소 합/minimum-sum-path-in-square.java
+++ 250318/정수 사각형 최소 합/minimum-sum-path-in-square.java
@@ -14,14 +14,18 @@ public class Main {
         
         dp = new int[n][n];
 
-        dp[n - 1][0] = matrix[n - 1][0];
-        dp[n - 1][1] = matrix[n - 1][1] + dp[n - 1][0];
-        dp[n - 1][2] = matrix[n - 1][2] + dp[n - 1][1];
+        // dp[n - 1][0] = matrix[n - 1][0];
 
-        for (int i = n - 2; i >= 0; i--){
-            dp[i][0] = dp[i + 1][0] + matrix[i][0];
-            for (int j = 1; j < n; j++)
-                dp[i][j] = Math.min(dp[i + 1][j], dp[i][j - 1]) + matrix[i][j];
+        for (int i = n - 1; i >= 0; i--){
+            for (int j = 0; j < n; j++){
+                if (i < n - 1 && j > 0)
+                    dp[i][j] = Math.min(dp[i + 1][j], dp[i][j - 1]);
+                else if (i == n - 1 && j > 0)
+                    dp[i][j] = dp[i][j - 1];
+                else if (j == 0 && i < n - 1)
+                    dp[i][j] = dp[i + 1][j];
+                dp[i][j] += matrix[i][j];
+            }
         }
 
         System.out.print(dp[0][n - 1]);

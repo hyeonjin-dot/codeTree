7662addd3d101b382ba75af81c79048fd1e68931
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-03-18 19:29:47 +0900

    Title: 동전 거슬러주기, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250318/동전 거슬러주기/coin-change.java
+++ 250318/동전 거슬러주기/coin-change.java
@@ -8,12 +8,25 @@ public class Main {
 
     public static int find(int num){
         int min = Integer.MAX_VALUE;
+
         for (int i = 0; i < n; i++){
-            if (num - coin[i] >= 0 && dp[num - coin[i]] != 0){
+            if (num - coin[i] >= 0 && dp[num - coin[i]] != 0)
                 min = Math.min(dp[num - coin[i]], min);
-            }
         }
         return min;
+    }
+
+    public static void makeDp(){
+        for (int i = 0; i < n; i++){
+            for (int j = coin[i]; j <= m; j++){
+                if (dp[j - coin[i]] != 0){
+                    if (dp[j] == 0)
+                        dp[j] = dp[coin[i]] + 1;
+                    else
+                        dp[j] = Math.min(dp[j], dp[coin[i]] + 1);
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -29,6 +42,7 @@ public class Main {
         }
 
         dp[0] = 0;
+        makeDp();
         int res = find(m);
         if (res == Integer.MAX_VALUE)
             res = -2;

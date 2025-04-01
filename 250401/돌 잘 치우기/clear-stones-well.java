56b2dd6ae64d0bee47fc11fe5de052ad5f109321
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-04-01 17:38:17 +0900

    Title: 돌 잘 치우기, Time: 129ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250401/돌 잘 치우기/clear-stones-well.java
+++ 250401/돌 잘 치우기/clear-stones-well.java
@@ -10,22 +10,63 @@ public class Main {
     public static int[] dx = {0, 0, 1, -1};
     public static int[] dy = {1, -1, 0, 0};
     public static Queue<int[]> q = new LinkedList<>();
-    public static List<int[]> lst;
+    public static List<int[]> lst = new ArrayList<>();
+    public static List<int[]> rock;
+    public static int cnt;
+
+    public static void find(int idx){
+        if (rock.size() == m){
+            for (int i = 0; i < k; i++){
+                q.add(new int[]{startPoints[i][0], startPoints[i][1]});
+                visited = new boolean[n][n];
+                visited[startPoints[i][0]][startPoints[i][1]] = true;
+                bfs();
+                cnt = Math.max(cnt, countVisited());
+            }
+            return ;
+        }
+
+        if (idx == lst.size())
+            return;
+        
+
+        rock.add(lst.get(idx));
+        find(idx + 1);
+        rock.remove(rock.size() - 1);
+        find(idx + 1);
+    }
+
+    public static void foundRock(){
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                if (grid[i][j] == 1)
+                    lst.add(new int[]{i, j});
+            }
+        }
+    }
 
     public static boolean inRange(int x, int y){
         return (x >= 0 && x < n && y >= 0 && y < n);
     }
 
     public static boolean canGo(int x, int y){
-        if (lst.size() == m){
-            if (grid[x][y] == 1)
-                return false;
-        }else {
-            if (grid[x][y] == 1)
-                lst.add(new int[]{x, y});
-        }
+        // if (lst.size() == m){
+        //     if (grid[x][y] == 1)
+        //         return false;
+        // }else {
+        //     if (grid[x][y] == 1)
+        //         lst.add(new int[]{x, y});
+        // }
 
-        return true;
+        if (grid[x][y] == 1){
+            for (int[] r : rock) {
+                if (r[0] == x && r[1] == y)
+                    return true;
+            }
+        }else
+            return true;
+
+        return false;
     }
 
     public static int countVisited(){
@@ -74,17 +115,22 @@ public class Main {
             startPoints[i][0] = sc.nextInt() - 1;
             startPoints[i][1] = sc.nextInt() - 1;
         }
-        
-        int cnt = 0;
 
-        for (int i = 0; i < k; i++){
-            lst = new ArrayList<>();
-            q.add(new int[]{startPoints[i][0], startPoints[i][1]});
-            visited = new boolean[n][n];
-            visited[startPoints[i][0]][startPoints[i][1]] = true;
-            bfs();
-            cnt = Math.max(cnt, countVisited());
-        }
+        foundRock();
+        rock = new ArrayList<>();
+        cnt = 0;
+        find(0);
+        
+        // int cnt = 0;
+
+        // for (int i = 0; i < k; i++){
+        //     q.add(new int[]{startPoints[i][0], startPoints[i][1]});
+        //     visited = new boolean[n][n];
+        //     visited[startPoints[i][0]][startPoints[i][1]] = true;
+        //     rock = new ArrayList<>();
+        //     bfs();
+        //     cnt = Math.max(cnt, countVisited());
+        // }
 
         System.out.print(cnt);
 

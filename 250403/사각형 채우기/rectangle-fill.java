8e3d62189c06744c8d2b42a1a604a66db64b3dbf
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-04-03 16:54:57 +0900

    Update rectangle-fill.java

--- 250403/사각형 채우기/rectangle-fill.java
+++ 250403/사각형 채우기/rectangle-fill.java
@@ -1,12 +1,16 @@
 import java.util.*;
 public class Main {
+    public static int[] dp = new int[1001];
+
     public static int fibo(int n){
-        if (n == 1)
-            return 1;
-        else if (n == 2)
-            return 2;
-        else
-            return (fibo(n - 1) + fibo(n - 2)) % 10007;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+        
+        for (int i = 4; i <= n; i++)
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        
+        return dp[n];
     }
 
     public static void main(String[] args) {

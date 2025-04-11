7c91ada0278b2d673963bbd6517dabf28f2a4e9d
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-04-11 14:27:52 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 2271ms, Memory: 176MB, Status: Memory Limit Exceed - Codetree

---
+++ 250411/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Main {
+    public static int n;
+    public static Queue<int[]> q = new LinkedList<>();
+
+    public static int calculation(int idx, int n){
+        if (idx == 0)
+            return n - 1;
+        else if (idx == 1)
+            return n + 1;
+        else if (idx == 2 && n % 2 == 0)
+            return n / 2;
+        else if (idx == 3 && n % 3 == 0)
+            return n / 3;
+        return n;
+    }
+
+    public static int dfs(){
+        while (!q.isEmpty()){
+            int[] tmp = q.poll();
+            int x = tmp[0];
+            int move = tmp[1];
+
+            if (x == 1)
+                return move;
+            
+            for (int i = 0; i < 4; i++){
+                int nx = calculation(i, x);
+                if (nx == n)
+                    continue;
+                q.add(new int[]{nx, move + 1});
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        q.add(new int[]{n, 0});
+        System.out.print(dfs());
+        
+    }
+}

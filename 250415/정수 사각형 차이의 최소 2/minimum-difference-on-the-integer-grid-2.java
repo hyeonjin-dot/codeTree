b8a229b030c1280967983358d825b06c333cf7c4
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-04-15 12:39:46 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 126ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250415/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.java
+++ 250415/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.java
@@ -4,8 +4,8 @@ public class Main {
     public static int n;
     public static int[][] grid;
     public static int[][][] dp;
-    // public static int min;
-    // public static int max;
+    public static int min;
+    public static int max;
     public static int[] dx = {1, 0};
     public static int[] dy = {0, 1};
 
@@ -14,24 +14,26 @@ public class Main {
     }
 
     public static void findDP(int x, int y){
+        if (dp[x][y] == null)
+            dp[x][y] = new int[]{grid[x][y], grid[x][y]};
+
         for (int i = 0; i < 2; i++){
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if (inRange(nx, ny)){
-                int[] tmp = dp[x][y];
-                int min;
-                int max;
-                if (tmp == null){
-                    min = Math.min(grid[x][y], grid[nx][ny]);
-                    max = Math.max(grid[x][y],grid[nx][ny]);
-                }
-                else{
-                    min = Math.min(tmp[0], grid[nx][ny]);
-                    max = Math.max(tmp[1], grid[nx][ny]);
-                }
-                int[] check = dp[nx][ny];
-                if (check == null || max - min < Math.abs(check[1] - check[0]))
-                    dp[nx][ny] = new int[]{min, max};
+            if (!inRange(nx, ny)) continue;
+
+            // 현재까지 min/max
+            int curMin = dp[x][y][0];
+            int curMax = dp[x][y][1];
+
+            int nextVal = grid[nx][ny];
+            int newMin = Math.min(curMin, nextVal);
+            int newMax = Math.max(curMax, nextVal);
+
+            int[] next = dp[nx][ny];
+            if (next == null || newMax - newMin < next[1] - next[0]) {
+                dp[nx][ny] = new int[]{newMin, newMax};
+                findDP(nx, ny);
             }
         }
     }
@@ -48,17 +50,10 @@ public class Main {
                 grid[i][j] = sc.nextInt();
         }
 
-        // dp[n - 1][n - 1] = 0;
-        // min = grid[n - 1][n - 1];
-        // max = grid[n - 1][n - 1];
+        findDP(0, 0);
 
-        dp[0][0] = new int[]{grid[0][0], grid[0][0]};
-
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < n; j++)
-                findDP(i, j);
-        }
         int[] tmp = dp[n - 1][n - 1];
         System.out.print(Math.abs(tmp[0] - tmp[1]));
+
     }
 }

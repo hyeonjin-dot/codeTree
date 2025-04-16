599ea224f917d923820a4598bbfe20b6fcb60294
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-04-16 11:33:41 +0900

    Title: 2차원 최대 증가 수열, Time: 152ms, Memory: 10MB, Status: Passed - Codetree

--- 250416/2차원 최대 증가 수열/longest-increasing-sequence-2d.java
+++ 250416/2차원 최대 증가 수열/longest-increasing-sequence-2d.java
@@ -5,7 +5,6 @@ public class Main {
     public static int m;
     public static int[][] grid;
     public static int[][] dp;
-    public static boolean[][] visited;
     public static int max;
 
     public static boolean inRange(int x, int y){
@@ -13,14 +12,13 @@ public class Main {
     }
 
     public static void dfs(int x, int y){
-        if (!inRange(x, y) || visited[x][y] || x == n - 1 || y == m - 1)
+        if (!inRange(x, y) || x == n - 1 || y == m - 1)
             return ;
 
         for (int i = x + 1; i < n; i++){
             for (int j = y + 1; j < m; j++){
-                if (!visited[i][j] && grid[x][y] < grid[i][j]){
+                if (grid[x][y] < grid[i][j]){
                     dp[i][j] = Math.max(dp[x][y] + 1, dp[i][j]);
-                    visited[i][j] = true;
                     max = Math.max(max, dp[i][j]);
                     dfs(i, j);
                 }
@@ -42,21 +40,29 @@ public class Main {
             }
         }
 
-        // dp[n - 1][m - 1] = 1;
-
-        // for (int i = n - 2; i >= 0; i--){
-        //     for (int j = m - 2; j >=0; j--){
-        //         if (grid[i][j] < grid[n - 1][m - 1])
-        //             dp[i][j] = dp[n - 1][m - 1] + 1;
-        //     }
-        // }
-
-        visited = new boolean[n][m];
-        visited[0][0] = true;
         dp[0][0] = 1;
-        max = 1;
-        dfs(0, 0);
-        
+
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                for (int x = 0; x < i; x++){
+                    for (int y = 0; y < j; y++){
+                        if (dp[x][y] == -1)
+                            continue;
+                        
+                        if (grid[i][j] > grid[x][y])
+                            dp[i][j] = Math.max(dp[x][y] + 1, dp[i][j]);
+                    }
+                }
+            }
+        }
+
+        max = 0;
+
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++)
+                max = Math.max(max, dp[i][j]);
+        }
+
         System.out.print(max);
 
     }

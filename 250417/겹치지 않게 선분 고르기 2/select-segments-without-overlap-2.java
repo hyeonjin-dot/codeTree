a4685eaabdf44eaa57535b6db5408b56e87e6ceb
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-04-17 11:52:28 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 389ms, Memory: 17MB, Status: Passed - Codetree

--- 250417/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.java
+++ 250417/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.java
@@ -16,7 +16,7 @@ public class Main {
         for (int i = 0; i < n; i++){
             for (int j = 0; j < i; j++){
                 if (segments[i][0] > segments[j][1])
-                    dp[i] = dp[j] + 1;
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
             }
         }
 

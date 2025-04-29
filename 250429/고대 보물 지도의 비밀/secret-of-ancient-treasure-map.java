12817a293f0ba83a86a3ccbb0152137642e04762
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-04-29 11:22:06 +0900

    Title: 고대 보물 지도의 비밀, Time: 613ms, Memory: 32MB, Status: Wrong Answer - Codetree

--- 250429/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.java
+++ 250429/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.java
@@ -20,32 +20,47 @@ public class Main {
             dp[0][1] = arr[0]; // 처음이 음수면 1번 사용한 것으로 시작
         }
 
-        int answer = arr[0]; // 최대합 초기값
+        // int answer = arr[0]; // 최대합 초기값
 
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j <= k; j++) {
-                if (dp[i-1][j] == Integer.MIN_VALUE) continue;
+        // for (int i = 1; i < n; i++) {
+        //     for (int j = 0; j <= k; j++) {
+        //         if (dp[i-1][j] == Integer.MIN_VALUE) continue;
 
-                if (arr[i] >= 0) {
-                    // 양수: 그냥 이어서 더하기 or 새로 시작
-                    dp[i][j] = Math.max(dp[i][j], dp[i-1][j] + arr[i]);
-                    dp[i][0] = Math.max(dp[i][0], arr[i]);
-                } else {
-                    if (j + 1 <= k) {
-                        // 음수: j+1로 관리
-                        dp[i][j+1] = Math.max(dp[i][j+1], dp[i-1][j] + arr[i]);
-                        dp[i][j+1] = Math.max(dp[i][j+1], arr[i]);
-                    }
+        //         if (arr[i] >= 0) {
+        //             // 양수: 그냥 이어서 더하기 or 새로 시작
+        //             dp[i][j] = Math.max(dp[i][j], dp[i-1][j] + arr[i]);
+        //             dp[i][0] = Math.max(dp[i][0], arr[i]);
+        //         } else {
+        //             if (j + 1 <= k) {
+        //                 // 음수: j+1로 관리
+        //                 dp[i][j+1] = Math.max(dp[i][j+1], dp[i-1][j] + arr[i]);
+        //                 dp[i][j+1] = Math.max(dp[i][j+1], arr[i]);
+        //             }
+        //         }
+        //     }
+        // }
+
+        int ans = arr[0];
+        for (int i = 1; i < n; i++){
+            if (arr[i] >= 0) {
+                for(int j = 0; j <= k; j++) {
+                    dp[i][j] = Math.max(dp[i - 1][j] + arr[i], arr[i]);
+                    ans = Math.max(ans, dp[i][j]);
                 }
+            } else {
+                for (int j = 0; j < k; j++)
+                    dp[i][j + 1] = Math.max(dp[i][j + 1], dp[i - 1][j] + arr[i]);
             }
+
         }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= k; j++) {
-                answer = Math.max(answer, dp[i][j]);
-            }
-        }
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j <= k; j++) {
+        //         System.out.print(dp[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
 
-        System.out.println(answer);
+        System.out.println(ans);
     }
 }

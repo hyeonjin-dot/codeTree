06fc2f8c9d1cac74e19fe77d04e052ea8961db02
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-04-30 16:25:11 +0900

    Title: 올바른 등식 만들기, Time: 130ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250430/올바른 등식 만들기/right-equality.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = sc.nextInt();
+        }
+        long[][] dp = new long[n][41];
+
+        dp[0][numbers[0] + 20] = 1;
+        dp[0][20 - numbers[0]] = 1;
+
+        for (int i = 1; i < n; i++){
+            for (int j = 0; j < 41; j++){
+                if (dp[i - 1][j] == 0)
+                    continue;
+
+                if (j + numbers[i] < 41){
+                    dp[i][j + numbers[i]] += dp[i - 1][j];
+                }
+                if (j - numbers[i] >= 0){
+                    dp[i][j - numbers[i]] += dp[i - 1][j];
+                }
+            }
+        }
+
+        System.out.print(dp[n - 1][m + 20]);
+
+    }
+}

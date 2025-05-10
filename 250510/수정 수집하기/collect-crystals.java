a080f1a2030a7c80810aca0e637dab2cc1587403
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-05-10 19:04:55 +0900

    Title: 수정 수집하기, Time: 168ms, Memory: 11MB, Status: Passed - Codetree

--- 250510/수정 수집하기/collect-crystals.java
+++ 250510/수정 수집하기/collect-crystals.java
@@ -5,36 +5,36 @@ public class Main {
         int n = sc.nextInt();
         int k = sc.nextInt();
         String s = sc.next();
-        int len = s.length();
-        int[] arr = new int[len];
-        for (int i = 0; i < len; i++){
-            if (s.charAt(i) == 'L')
+        int[] arr = new int[n + 1];
+        for (int i = 1; i <= n; i++){
+            if (s.charAt(i - 1) == 'L')
                 arr[i] = 0;
             else
                 arr[i] = 1;
         }
         
-        int[][][] dp = new int[n][2][k + 1]; // 방향 바꾼 수 1 ~ k
+        int[][][] dp = new int[n + 1][2][k + 1]; // 방향 바꾼 수 1 ~ k
 
-        if (arr[0] == 0)
-            dp[0][0][1] = 1;
+        if (arr[1] == 0)   
+            dp[1][0][0] = 1;
         else
-            dp[0][1][1] = 1;
+            dp[1][1][1] = 1;
+        
 
-        for (int i = 1; i < n; i++){
-            for (int c = 1; c <= k; c++){
+        for (int i = 2; i <= n; i++){
+            for (int c = 0; c <= k; c++){
                 for (int d = 0; d < 2; d++){
                     if (arr[i] == d) {
                         // 같은 방향 유지
                         dp[i][d][c] = Math.max(dp[i][d][c], dp[i - 1][d][c] + 1);
                         // 다른 방향에서 전환해서 옴
-                        if (c > 1)
+                        if (c > 0)
                             dp[i][d][c] = Math.max(dp[i][d][c], dp[i - 1][1 - d][c - 1] + 1);
                     } else {
                         // 같은 방향에서 그냥 무시 (전환 안 씀)
                         dp[i][d][c] = Math.max(dp[i][d][c], dp[i - 1][d][c]);
                         // 다른 방향에서 전환해서 넘어옴
-                        if (c > 1)
+                        if (c > 0)
                             dp[i][d][c] = Math.max(dp[i][d][c], dp[i - 1][1 - d][c - 1] + 1);
                     }
                 }
@@ -42,7 +42,7 @@ public class Main {
         }
 
         int result = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i <= n; i++) {
             for (int d = 0; d < 2; d++) {
                 for (int c = 0; c <= k; c++) {
                     result = Math.max(result, dp[i][d][c]);

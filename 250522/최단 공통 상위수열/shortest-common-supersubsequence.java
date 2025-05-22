aa8a254571cfb05d49d35953844629b743ca3b69
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-05-22 16:09:43 +0900

    Title: 최단 공통 상위수열, Time: 178ms, Memory: 13MB, Status: Passed - Codetree

--- 250522/최단 공통 상위수열/shortest-common-supersubsequence.java
+++ 250522/최단 공통 상위수열/shortest-common-supersubsequence.java
@@ -1,29 +1,29 @@
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
         String t = sc.next();
+
         int sLen = s.length();
         int tLen = t.length();
 
         int[][] dp = new int[sLen + 1][tLen + 1];
-        for (int i = 0; i <= sLen; i++)
-            dp[i][0] = i;
-        for (int j = 0; j <= tLen; j++)
-            dp[0][j] = j; 
 
-        for (int i = 1; i <= sLen; i++){
-            for (int j = 1; j <= tLen; j++){
+        // LCS 계산
+        for (int i = 1; i <= sLen; i++) {
+            for (int j = 1; j <= tLen; j++) {
                 if (s.charAt(i - 1) == t.charAt(j - 1))
                     dp[i][j] = dp[i - 1][j - 1] + 1;
-                else {
-                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + 1;
-                    dp[i][j] = Math.min(dp[i][j], dp[i - 1][j - 1] + 2);
-                }
+                else
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
             }
         }
 
-        System.out.print(dp[sLen][tLen]);
+        int lcsLen = dp[sLen][tLen];
+        int scsLen = sLen + tLen - lcsLen;
+
+        System.out.println(scsLen);
     }
 }

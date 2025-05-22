35a386f7db121c14f0930ad0ae77c77d438ad9e7
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-05-22 16:05:55 +0900

    Title: 최단 공통 상위수열, Time: 144ms, Memory: 13MB, Status: Passed - Codetree

--- 250522/최단 공통 상위수열/shortest-common-supersubsequence.java
+++ 250522/최단 공통 상위수열/shortest-common-supersubsequence.java
@@ -8,13 +8,19 @@ public class Main {
         int tLen = t.length();
 
         int[][] dp = new int[sLen + 1][tLen + 1];
+        for (int i = 0; i <= sLen; i++)
+            dp[i][0] = i;
+        for (int j = 0; j <= tLen; j++)
+            dp[0][j] = j; 
 
         for (int i = 1; i <= sLen; i++){
             for (int j = 1; j <= tLen; j++){
                 if (s.charAt(i - 1) == t.charAt(j - 1))
                     dp[i][j] = dp[i - 1][j - 1] + 1;
-                else
-                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + 2;
+                else {
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + 1;
+                    dp[i][j] = Math.min(dp[i][j], dp[i - 1][j - 1] + 2);
+                }
             }
         }
 

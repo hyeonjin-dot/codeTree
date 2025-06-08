03e127ab7bccdbfe30376969fae41ff487f88c2f
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-06-08 22:22:25 +0900

    Title: 두 수의 합, Time: 785ms, Memory: 34MB, Status: Passed - Codetree

--- 250608/두 수의 합/sum-of-two-num.java
+++ 250608/두 수의 합/sum-of-two-num.java
@@ -1,20 +1,23 @@
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         long k = sc.nextLong();
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
         
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        
+        Map<Long, Integer> map = new HashMap<>();
         int cnt = 0;
-        for (int i = 0; i < n - 1; i++){
-            for (int j = i + 1; j < n; j++){
-                if (arr[i] + arr[j] == k)
-                    cnt++;
-            }
+
+        for (int i = 0; i < n; i++) {
+            long target = k - arr[i];
+            cnt += map.getOrDefault(target, 0); // 앞에 target 값이 몇 번 나왔는지
+
+            map.put((long)arr[i], map.getOrDefault((long)arr[i], 0) + 1); // 현재 숫자 기록
         }
 
         System.out.print(cnt);

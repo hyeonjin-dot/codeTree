2b5bc66f98f46528432c4f43dcd56cd93665fa65
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-06-09 13:14:09 +0900

    Title: 세 수의 합, Time: 130ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250609/세 수의 합/sum-of-three-num.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long k = sc.nextLong();
+        int[] arr = new int[n];
+        HashMap<Long, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        int cnt = 0;
+        
+        for (int i = 0; i < n - 1; i++){
+            for (int j = i + 1; j < n; j++){
+                long sum = (long)(arr[i] + arr[j]);
+                long target = k - sum;
+                cnt += map.getOrDefault(target, 0);
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+        }
+
+        System.out.print(cnt);
+    }
+}

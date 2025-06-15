3b8aa7e74583013bed867feef9a9813665a6e2dd
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-06-15 20:38:33 +0900

    Title: 정수 N개의 합 3, Time: 142ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250615/정수 N개의 합 3/sum-of-n-integers-3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        // Please write your code here.
+    }
+}

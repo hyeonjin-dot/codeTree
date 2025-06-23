b26358cbb1d66f1fc9a65e799fb3597f409e8a5a
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-06-23 20:32:15 +0900

    Title: 비율 구하기, Time: 658ms, Memory: 23MB, Status: Wrong Answer - Codetree

---
+++ 250623/비율 구하기/find-proportions.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.util.Map.Entry;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        TreeMap<String, Integer> map = new TreeMap<>();
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.next();
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+
+        Iterator<Entry<String, Integer>> it = map.entrySet().iterator();
+        while (it.hasNext()){
+            Entry<String, Integer> entry = it.next();
+            // System.out.println(entry.getKey() + " " + );
+            System.out.printf("%s %.4f\n", entry.getKey(), (double)(entry.getValue() * 100 / n));
+        }
+        // Please write your code here.
+    }
+}

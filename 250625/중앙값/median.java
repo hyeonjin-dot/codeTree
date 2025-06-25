8b0f25db2300c9e4a498318919ec48871f29733b
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-06-25 10:52:17 +0900

    Title: 중앙값, Time: 3091ms, Memory: 36MB, Status: Time Limit Exceed - Codetree

--- 250625/중앙값/median.java
+++ 250625/중앙값/median.java
@@ -5,20 +5,24 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
         while(t-- > 0) {
-            PriorityQueue<Integer> q = new PriorityQueue<>();
+            // PriorityQueue<Integer> q = new PriorityQueue<>();
             int m = sc.nextInt();
             int[] arr = new int[m];
+            List<Integer> lst = new ArrayList<>();
             for(int i = 0; i < m; i++){
                 arr[i] = sc.nextInt();
-                q.add(arr[i]);
+                lst.add(arr[i]);
+                // q.add(arr[i]);
                 if (i % 2 == 0){
-                    int tmp = i / 2;
-                    PriorityQueue<Integer> qq = new PriorityQueue<>(Collections.reverseOrder());
-                    for (int j = 0; j <= tmp; j++)
-                        qq.add(q.poll());
-                    System.out.print(qq.peek() + " ");
-                    while (qq.size() > 0)
-                        q.add(qq.poll());
+                    Collections.sort(lst);
+                    System.out.print(lst.get(i / 2) + " ");
+                    // int tmp = i / 2;
+                    // PriorityQueue<Integer> qq = new PriorityQueue<>(Collections.reverseOrder());
+                    // for (int j = 0; j <= tmp; j++)
+                    //     qq.add(q.poll());
+                    // System.out.print(qq.peek() + " ");
+                    // while (qq.size() > 0)
+                    //     q.add(qq.poll());
                 }
             }
             System.out.println();

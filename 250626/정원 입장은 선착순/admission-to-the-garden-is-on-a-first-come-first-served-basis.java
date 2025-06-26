76b38c203d47fb24cba2f0c5790070a494c66314
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-06-26 15:42:06 +0900

    Title: 정원 입장은 선착순, Time: 285ms, Memory: 15MB, Status: Runtime Error - Codetree

---
+++ 250626/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.java
@@ -0,0 +1,46 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        PriorityQueue<int[]> q = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+        // int[][] a = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            int[] a = new int[3];
+            a[0] = sc.nextInt();
+            a[1] = sc.nextInt();
+            a[2] = i;
+            q.add(a);
+        }
+        
+        int[] a = q.poll();
+        int sum = a[0] + a[1];
+        int max = 0;
+
+        while (q.size() > 0){
+            List<int[]> lst = new ArrayList<>();
+            while (true){
+                int[] tmp = q.poll();
+                if (tmp[0] > sum){
+                    q.add(tmp);
+                    break ;
+                }
+                lst.add(tmp);
+            }
+            
+            int[] tmp;
+            if (lst.size() > 0){
+                Collections.sort(lst, Comparator.comparingInt(l -> l[2]));
+                tmp = lst.get(0);
+            } else
+                tmp = q.poll();
+
+            max = Math.max(max, sum - tmp[0]);
+            sum += tmp[1];
+            for (int i = 1; i < lst.size(); i++)
+                q.add(lst.get(i));
+        }
+
+        System.out.println(max);
+    }
+}

5a0c5e437cccdb3beea7ebb4dcdd25ef6ce16222
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-06-29 14:05:04 +0900

    Title: 정수 명령어, Time: 132ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250629/정수 명령어/Integer-command.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        TreeSet<Integer> s = new TreeSet<>();
+        for (int tc = 0; tc < t; tc++) {
+            int n = sc.nextInt();
+            for (int i = 0; i < n; i++) {
+                char command = sc.next().charAt(0);
+                int num = sc.nextInt();
+                if (command == 'I')
+                    s.add(num);
+                else if (command == 'D' && !s.isEmpty()){
+                    if (num == 1)
+                        s.remove(s.last());
+                    else
+                        s.remove(s.first());
+                }
+            }
+            if (s.isEmpty())
+                System.out.println("EMPTY");
+            else
+                System.out.println(s.last() + " " + s.first());
+        }
+    }
+}

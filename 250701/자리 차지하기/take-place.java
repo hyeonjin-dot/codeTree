1e55fd8c16341c4acc833602bc031cb96f36e839
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-07-01 13:58:52 +0900

    Title: 자리 차지하기, Time: 3058ms, Memory: 133MB, Status: Time Limit Exceed - Codetree

---
+++ 250701/자리 차지하기/take-place.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        TreeSet<Integer> s = new TreeSet<>();
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            if (!s.contains(arr[i]))
+                s.add(arr[i]);
+            else {
+                int tmp = arr[i];
+                while (tmp > 0){
+                    if (!s.contains(tmp)){
+                        s.add(tmp);
+                        break;
+                    }
+                    tmp--;
+                }
+            }
+        }
+
+        System.out.print(s.size());
+        // Please write your code here.
+    }
+}

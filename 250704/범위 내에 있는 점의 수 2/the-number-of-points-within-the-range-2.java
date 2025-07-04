000c4c7f92065b6fb9191a3dbf474ad6cf05ee7c
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-07-04 14:34:23 +0900

    Title: 범위 내에 있는 점의 수 2, Time: 1651ms, Memory: 60MB, Status: Passed - Codetree

--- 250704/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
+++ 250704/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
@@ -5,20 +5,25 @@ public class Main {
         int n = sc.nextInt();
         int q = sc.nextInt();
         int[] points = new int[1000001];
+        int[] prexSum = new int[1000001];
         for (int i = 0; i < n; i++) {
            int idx = sc.nextInt();
             points[idx] = 1;
         }
+
+        prexSum[0] = points[0];
+        for (int i = 1; i < 1000001; i++)
+            prexSum[i] = prexSum[i - 1] + points[i];
+        
+
         for (int i = 0; i < q; i++) {
             int a = sc.nextInt();
             int b = sc.nextInt();
-            
-            int cnt = 0;
-            for (int j = a; j <= b; j++){
-                if (points[j] == 1)
-                    cnt++;
-            }
-            System.out.println(cnt);
+
+            if (a > 0)
+                System.out.println(prexSum[b] - prexSum[a - 1]);
+            else
+                System.out.println(prexSum[b]);
         }
     }
 }

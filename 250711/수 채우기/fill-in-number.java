5e66bab1fe56d0b42bbadf75463c6db19b26defb
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-07-11 14:52:09 +0900

    Title: 수 채우기, Time: 129ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250711/수 채우기/fill-in-number.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = n / 5;
+        n = n % 5;
+        
+        if (n % 2 == 0)
+            System.out.print(cnt + (n / 2));
+        else {
+            n += 5;
+            cnt--;
+            if (n % 2 == 0)
+                System.out.print(cnt + (n / 2));
+            else
+                System.out.print(-1);
+        }
+    }
+}

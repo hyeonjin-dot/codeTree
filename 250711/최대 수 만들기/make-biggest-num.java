f1d664df19a30822db9d3b774e60dd227ecf37de
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-07-11 14:49:17 +0900

    Title: 최대 수 만들기, Time: 1055ms, Memory: 88MB, Status: Passed - Codetree

--- 250711/최대 수 만들기/make-biggest-num.java
+++ 250711/최대 수 만들기/make-biggest-num.java
@@ -8,23 +8,8 @@ public class Main {
             arr[i] = sc.next().trim();
         }
 
-        Arrays.sort(arr, (a, b) -> {
-            // 첫 번째 문자 비교
-            if (a.charAt(0) != b.charAt(0)) {
-                return Character.compare(b.charAt(0), a.charAt(0));
-            }
-            // 길이 비교
-            else if (a.length() != b.length()) {
-                return Integer.compare(a.length(), b.length());
-            }
-            // 두 번째 문자 비교
-            else {
-                // 만약 문자열 길이가 1이라면 비교할 수 없음
-                if (a.length() < 2 || b.length() < 2) return 0;
-                return Character.compare(b.charAt(1), a.charAt(1));
-            }
-        });
-        
+        Arrays.sort(arr, (a, b) -> (b + a).compareTo(a + b));
+
         for (int i = 0; i < n; i++){
             System.out.print(arr[i]);
         }

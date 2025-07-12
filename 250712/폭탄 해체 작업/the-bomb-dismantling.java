ecd227a9b9aca0e22234b1d6666170c64eebd7ed
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-07-12 16:08:43 +0900

    Title: 폭탄 해체 작업, Time: 139ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250712/폭탄 해체 작업/the-bomb-dismantling.java
+++ 250712/폭탄 해체 작업/the-bomb-dismantling.java
@@ -1,27 +1,60 @@
 import java.util.*;
+
+class Pair implements Comparable<Pair> {
+    int time;
+    int value;
+
+    public Pair(int time, int value){
+        this.time = time;
+        this.value = value;
+    }
+
+    @Override
+    public int compareTo(Pair o) {
+        if (this.time != o.time) return Integer.compare(this.time, o.time);
+        return Integer.compare(this.value, o.value);
+    }
+
+    public int getValue(){
+        return this.value;
+    }
+
+    public int getTime(){
+        return this.time;
+    }
+
+}
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] scores = new int[n];
         int[] timeLimits = new int[n];
-        Map<Integer, Integer> m = new HashMap<>();
+        TreeSet<Pair> m = new TreeSet<>();
+        int time = 0;
         for (int i = 0; i < n; i++) {
             scores[i] = sc.nextInt();
             timeLimits[i] = sc.nextInt();
-            if (!m.containsKey(timeLimits[i]))
-                m.put(timeLimits[i], scores[i]);
-            else {
-                if (m.get(timeLimits[i]) < scores[i])
-                    m.put(timeLimits[i], scores[i]);
-            }
+            m.add(new Pair(timeLimits[i], scores[i]));
+            time = Math.max(time, timeLimits[i]);
         }
 
+        int idx = 1;
         int res = 0;
-        List<Integer> lst = new ArrayList<>(m.keySet());
-        for (int key : lst){
-            res += m.get(key);
+        while (idx <= time){
+            if (m.lower(new Pair(idx + 1, 0)) != null){
+                Pair tmp = m.lower(new Pair(idx + 1, 0));
+                // System.out.println(idx + " " + tmp.getTime() + " " + tmp.getValue());
+                if (tmp.getTime() >= idx) {
+                    res += tmp.getValue();
+                    m.remove(tmp);
+                }
+            }
+            idx++;
         }
+        
+        
         System.out.print(res);
 
     }

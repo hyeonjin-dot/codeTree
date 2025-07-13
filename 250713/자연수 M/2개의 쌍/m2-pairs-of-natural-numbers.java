49ac93ff771ec7c2510de59a7590af1cb40fdb89
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-07-13 15:08:03 +0900

    Title: 자연수 M/2개의 쌍, Time: 1209ms, Memory: 176MB, Status: Memory Limit Exceed - Codetree

--- 250713/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.java
+++ 250713/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.java
@@ -1,48 +1,36 @@
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        TreeMap<Integer, Integer> map = new TreeMap<>();
         int total = 0;
-        Map<Integer, Integer> m = new HashMap<>();
 
         for (int i = 0; i < n; i++) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-            m.put(y, m.getOrDefault(y, 0) + x);
-            total += x;
+            int count = sc.nextInt();
+            int value = sc.nextInt();
+            map.put(value, map.getOrDefault(value, 0) + count);
+            total += count;
         }
-        
-        int[] key = m.keySet().stream().mapToInt(i -> i).sorted().toArray();
 
-        int size = key.length;
-        int rightIdx = 0;
-        int leftIdx = size - 1;
-        int rightCnt = m.get(key[rightIdx]);
-        int leftCnt = m.get(key[leftIdx]);
         int max = 0;
-        
-        for (int i = 0; i < total / 2; i++){
-            int sum = key[rightIdx] + key[leftIdx];
-            max = Math.max(max, sum);
 
-            rightCnt--;
-            leftCnt--;
+        while (total > 1) {
+            int minKey = map.firstKey();
+            int maxKey = map.lastKey();
+            int pairSum = minKey + maxKey;
+            max = Math.max(max, pairSum);
 
-            if (rightCnt <= 0){
-                rightIdx++;
-                rightCnt = m.get(key[rightIdx]);
-            }
-            if (leftCnt <= 0){
-                leftIdx--;
-                leftCnt = m.get(key[leftIdx]);;
-            }
+            // 하나씩 소모
+            map.put(minKey, map.get(minKey) - 1);
+            map.put(maxKey, map.get(maxKey) - 1);
+            total -= 2;
 
-            if (leftIdx <= rightIdx)
-                break;
-        }  
+            if (map.get(minKey) != null && map.get(minKey) == 0) map.remove(minKey);
+            if (map.get(maxKey) != null && map.get(maxKey) == 0 && minKey != maxKey) map.remove(maxKey);
+        }
 
-        System.out.print(max);
-        
+        System.out.println(max);
     }
 }

3a6089ab6a5891abd0d4580684d68e2fc2026ab7
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-07-13 15:05:55 +0900

    Title: 자연수 M/2개의 쌍, Time: 3073ms, Memory: 61MB, Status: Time Limit Exceed - Codetree

--- 250713/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.java
+++ 250713/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.java
@@ -3,24 +3,46 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        TreeSet<Integer> s = new TreeSet<>();
+        int total = 0;
+        Map<Integer, Integer> m = new HashMap<>();
+
         for (int i = 0; i < n; i++) {
             int x = sc.nextInt();
             int y = sc.nextInt();
-            for (int j = 0; j < x; j++)
-                s.add(y);
+            m.put(y, m.getOrDefault(y, 0) + x);
+            total += x;
         }
+        
+        int[] key = m.keySet().stream().mapToInt(i -> i).sorted().toArray();
 
-        int max = Integer.MIN_VALUE;
-        while (s.size() > 0){
-            int fir = s.first();
-            int last = s.last();
-            s.remove(fir);
-            s.remove(last);
-            max = Math.max(fir+last, max);
-        }
+        int size = key.length;
+        int rightIdx = 0;
+        int leftIdx = size - 1;
+        int rightCnt = m.get(key[rightIdx]);
+        int leftCnt = m.get(key[leftIdx]);
+        int max = 0;
+        
+        for (int i = 0; i < total / 2; i++){
+            int sum = key[rightIdx] + key[leftIdx];
+            max = Math.max(max, sum);
+
+            rightCnt--;
+            leftCnt--;
+
+            if (rightCnt <= 0){
+                rightIdx++;
+                rightCnt = m.get(key[rightIdx]);
+            }
+            if (leftCnt <= 0){
+                leftIdx--;
+                leftCnt = m.get(key[leftIdx]);;
+            }
+
+            if (leftIdx <= rightIdx)
+                break;
+        }  
 
         System.out.print(max);
-
+        
     }
 }

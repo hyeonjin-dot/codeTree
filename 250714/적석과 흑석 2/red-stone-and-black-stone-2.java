64a2ae518fd218e5af4f13cea5ae798cecfd30dd
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-07-14 14:35:32 +0900

    Title: 적석과 흑석 2, Time: 271ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250714/적석과 흑석 2/red-stone-and-black-stone-2.java
+++ 250714/적석과 흑석 2/red-stone-and-black-stone-2.java
@@ -15,12 +15,21 @@ public class Main {
         for (int i = 0; i < n; i++) {
             blackStones[i][0] = sc.nextInt();
             blackStones[i][1] = sc.nextInt();
+            
+        }
+
+        Arrays.sort(blackStones, (a, b) -> Integer.compare(a[1] - a[0], b[1] - b[0]));
+
+        for (int i = 0; i < n; i++){
             if (m.higher(blackStones[i][0]) != null 
                 && m.ceiling(blackStones[i][1]) != null){
-                    // if (m.higher(blackStones[i][0]) == m.ceiling(blackStones[i][1])){
-                        // m.remove(m.higher(blackStones[i][0]));
+                    if (m.higher(blackStones[i][0]) == m.ceiling(blackStones[i][1])){
+                        m.remove(m.higher(blackStones[i][0]));
                         cnt++;
-                    // }
+                    } else {
+                        m.remove(m.higher(blackStones[i][0]));
+                        cnt++;
+                    }
                 }
         }
 

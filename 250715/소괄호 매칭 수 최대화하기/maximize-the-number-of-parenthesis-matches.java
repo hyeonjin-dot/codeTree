83a726e9c69a8a6c6cb021d8dbc00a28ab4cff9a
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-07-15 12:45:54 +0900

    Title: 소괄호 매칭 수 최대화하기, Time: 699ms, Memory: 27MB, Status: Passed - Codetree

--- 250715/소괄호 매칭 수 최대화하기/maximize-the-number-of-parenthesis-matches.java
+++ 250715/소괄호 매칭 수 최대화하기/maximize-the-number-of-parenthesis-matches.java
@@ -1,31 +1,62 @@
 import java.util.*;
 
+class bck {
+    String s;
+    int open, close;
+
+    public bck(String s){
+        this.s = s;
+        int openCnt = 0;
+        int closeCnt = 0;
+        for (int i = 0; i < s.length(); i++){
+            if (s.charAt(i) == '(')
+                openCnt++;
+            else
+                closeCnt++;
+        }
+        this.open = openCnt;
+        this.close = closeCnt;
+    }
+
+    public int getClose(){
+        return this.close;
+    }
+
+    public int getOpen(){
+        return this.open;
+    }
+
+    public String getS(){
+        return this.s;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         String[] arr = new String[n];
+        bck[] lst = new bck[n];
         for (int i = 0; i < n; i++) {
             arr[i] = sc.next();
+            lst[i] = new bck(arr[i]);
         }
 
-        Arrays.sort(arr, (a, b) -> (a + b).compareTo(b + a));
+        Arrays.sort(lst, (a, b) -> a.getClose() * b.getOpen() - a.getOpen() * b.getClose());
 
         StringBuilder sb = new StringBuilder();
-        for (String s : arr) {
-            sb.append(s);
-        }
-        String str = sb.toString();
+        for (bck str : lst)
+            sb.append(str.getS());
+
         long cnt = 0;
         long back = 0;
-        for (int i = str.length() - 1; i >= 0; i--){
-            if (str.charAt(i) == ')')
-                back++;
-            if (str.charAt(i) == '(')
+        for (int i = sb.length() - 1; i >= 0; i--){
+            if (sb.charAt(i) == '(')
                 cnt += back;
+            else
+                back++;
         }
 
         System.out.print(cnt);
-        // Please write your code here.
     }
 }

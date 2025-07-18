d6faea3b7f5a8fff088cbef6ceb775c020d8a7cf
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2025-07-18 15:05:03 +0900

    Title: 씨 오 더블유, Time: 216ms, Memory: 14MB, Status: Passed - Codetree

--- 250718/씨 오 더블유/c-o-w.java
+++ 250718/씨 오 더블유/c-o-w.java
@@ -1,40 +1,26 @@
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         String str = sc.next();
-        int len = str.length();
-        int[] c = new int[len];
-        int[] o = new int[len];
-        int[] w = new int[len];
 
-        if (str.charAt(0) == 'C')
-            c[0] = 1;
-        else if (str.charAt(0) == 'O')
-            o[0] = 1;
-        else 
-            w[0] = 1;
-        
-        for (int i = 1; i < len; i++){
-            if (str.charAt(i) == 'C')
-                c[i] = c[i - 1] + 1;
-            else if (str.charAt(i) == 'O')
-                o[i] = o[i - 1] + 1;
-            else 
-                w[i] = w[i - 1] + 1;
-        }
+        long countC = 0;
+        long countCO = 0;
+        long countCOW = 0;
 
-        int res = 0;
-        for (int i = 0; i < len - 2; i++){
-            for (int j = i + 1; j < len - 1; j++){
-                for (int k = j + 1; k < len; k++){
-                    if (c[i] != 0 && o[j] != 0 && w[k] != 0)
-                        res++;
-                }
+        for (int i = 0; i < n; i++) {
+            char ch = str.charAt(i);
+            if (ch == 'C') {
+                countC++;
+            } else if (ch == 'O') {
+                countCO += countC;  // 지금까지의 C들을 이어받음
+            } else if (ch == 'W') {
+                countCOW += countCO;  // 지금까지의 CO들을 이어받음
             }
         }
 
-        System.out.print(res);
+        System.out.println(countCOW);
     }
 }
